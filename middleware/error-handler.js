0d314a53a82288a446438bf3013c959eb3e0144a
omar <herricheomar@gmail.com>
2024-02-07 15:46:25 +0100

    adding tothe packageJson

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -2,29 +2,24 @@ const { CustomApiError, NotFoundError } = require("../errors");
 const { StatusCodes } = require("http-status-codes");
 
 const errorHandlerMiddleWare = (err, req, res, next) => {
-  // const customError = {
-  //   errorMsg: err.msg || "something went wrong please try again later",
-  //   statusCode: err.statusCode || 500,
-  // };
-  // if (err.name === "ValidationError") {
-  //   customError.errorMsg=Object.values(err.errors)
-  //     .map((item) => item.message)
-  //     .join(',');
-  //   customError.statusCode = StatusCodes.BAD_REQUEST;
-  // }
-  // if (err.name === "CastError") {
-  //   customError.errorMsg=`there is no element with the id : ${err.value}`;
-  //   customError.statusCode = StatusCodes.NOT_FOUND;
-  // }
-  // if (err.code === 11000) {
-  //   customError.errorMsg = `this email: ${err.keyValue.email} is already taken use another one and it will work`;
-  //   customError.statusCode = StatusCodes.UNAUTHORIZED;
-  // }
-  if (err instanceof CustomApiError) {
-    return res.status(err.statusCode).json({ msg: err.message })
+  const customError = {
+    errorMsg: err.msg || "something went wrong please try again later",
+    statusCode: err.statusCode || 500,
+  };
+  if (err.name === "ValidationError") {
+    customError.errorMsg=Object.values(err.errors)
+      .map((item) => item.message)
+      .join(',');
+    customError.statusCode = StatusCodes.BAD_REQUEST;
   }
-
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
-  // return res.status(customError.statusCode).json({ msg: customError.errorMsg });
+  if (err.name === "CastError") {
+    customError.errorMsg=`there is no element with the id : ${err.value}`;
+    customError.statusCode = StatusCodes.NOT_FOUND;
+  }
+  if (err.code === 11000) {
+    customError.errorMsg = `this email: ${err.keyValue.email} is already taken use another one and it will work`;
+    customError.statusCode = StatusCodes.UNAUTHORIZED;
+  }
+  return res.status(customError.statusCode).json({ msg: customError.errorMsg });
 };
 module.exports = errorHandlerMiddleWare;
