f76bd34f7bd60f8bc669e6703bf10eef42cb7e05
omar <herricheomar@gmail.com>
2024-02-07 01:29:21 +0100

    l3iba

--- app.js
+++ app.js
@@ -12,17 +12,17 @@ const auth = require("./middleware/authentication");
 const cors=require('cors');
 const xss=require('xss-clean');
 const helmet=require('helmet');
-const rateLimiter=require('express-rate-limit');
+// const rateLimiter=require('express-rate-limit');
 
 //!importing : end
 
 const app = express();
-app.use(
-  rateLimiter({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
-  })
-);
+// app.use(
+//   rateLimiter({
+//     windowMs: 15 * 60 * 1000, // 15 minutes
+//     max: 100, // limit each IP to 100 requests per windowMs
+//   })
+// );
 app.use(cors()); //! first one
 app.use(xss());
 app.use(helmet());
